1f4403b55551d8eebead144f8dcd327b4ba7fad4
nikike <nikike@nikoletts-mbp.home>
2020-04-12 14:24:05 +0100

    Cleanup the code

--- src/battery_life/solution.py
+++ src/battery_life/solution.py
@@ -19,21 +19,3 @@ if __name__ == '__main__':
 
     res = (timeCharged * 2) if timeCharged <= 4 else max(y)
     print("{:.2f}".format(res))
-
-    # numx = np.array(x).reshape(-1, 1)
-    # numy = np.array(y).reshape(-1, 1)
-    #
-    # regressor = LinearRegression()
-    #
-    # regressor.fit(numx, numy)
-    #
-    # # intercept
-    # print(regressor.intercept_)
-    # # slope
-    # print(regressor.coef_)
-    #
-    # pred_y = regressor.predict(np.array(timeCharged).reshape(-1, 1))
-    # print(pred_y)
-    #
-    # pred_error = y - pred_y
-    # print(pred_error)
