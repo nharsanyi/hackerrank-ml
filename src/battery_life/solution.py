b9e3a47fbd96eedb905e796634754561294f5893
nikike <nikike@nikoletts-mbp.home>
2020-04-12 13:59:44 +0100

    Fix error

--- src/battery_life/solution.py
+++ src/battery_life/solution.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
@@ -11,23 +11,29 @@ if __name__ == '__main__':
     x = training_data[:, 0]
     y = training_data[:, 1]
 
+    plt.scatter(x, y)
+    plt.show()
+
     print(*x, sep=";")
     print(*y, sep=";")
 
-    numx = np.array(x).reshape(-1, 1)
-    numy = np.array(y).reshape(-1, 1)
+    res = (timeCharged * 2) if timeCharged <= 4 else max(y)
+    print("{:.2f}".format(res))
 
-    regressor = LinearRegression()
-
-    regressor.fit(numx, numy)
-
-    # intercept
-    print(regressor.intercept_)
-    # slope
-    print(regressor.coef_)
-
-    pred_y = regressor.predict(np.array(timeCharged).reshape(-1, 1))
-    print(pred_y)
-
-    pred_error = y - pred_y
-    print(pred_error)
+    # numx = np.array(x).reshape(-1, 1)
+    # numy = np.array(y).reshape(-1, 1)
+    #
+    # regressor = LinearRegression()
+    #
+    # regressor.fit(numx, numy)
+    #
+    # # intercept
+    # print(regressor.intercept_)
+    # # slope
+    # print(regressor.coef_)
+    #
+    # pred_y = regressor.predict(np.array(timeCharged).reshape(-1, 1))
+    # print(pred_y)
+    #
+    # pred_error = y - pred_y
+    # print(pred_error)
